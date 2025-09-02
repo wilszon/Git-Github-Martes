0abffac4bdfb95011d7d75c3f643450fe3eef1d2
Wilson Suarez <wsuarez874@unab.edu.co>
2025-09-02 14:57:50 -0500

    Detalle de un producto

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/detalle', function () {
+    return view('detalle');
+})->name('detalle');
